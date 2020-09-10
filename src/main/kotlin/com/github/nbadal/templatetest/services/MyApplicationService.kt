6a4815ec616a83eaafdb9220b9c91b10777b2b65
GitHub Action <action@github.com>
2020-09-10 17:17:23 +0000

    Template cleanup

--- src/main/kotlin/com/github/nbadal/templatetest/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/nbadal/templatetest/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.nbadal.templatetest.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.nbadal.templatetest.MyBundle
 
 class MyApplicationService {
 
